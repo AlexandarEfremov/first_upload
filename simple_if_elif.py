0eefd94f0d0a8c3a72f102c299049a69c269df32
AlexandarEfremov <efremov.alex@yahoo.bg>
2023-09-24 10:13:59 +0300

    first update

---
+++ simple_if_elif.py
@@ -0,0 +1,7 @@
+drink_choice = input("Would you like tea or coffee? ")
+if drink_choice.lower() == "tea":
+    print("Enjoy your tea")
+elif drink_choice.lower() == "coffee":
+    print("The best way to start a day is with a cuppa coffee")
+else:
+    print("Invalid input")
