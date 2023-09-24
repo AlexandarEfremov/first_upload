152fa10167e8ca9a10bf4a212cf2bb49255f6dff
AlexandarEfremov <efremov.alex@yahoo.bg>
2023-09-24 10:21:01 +0300

    ewcia login

---
+++ ewcia.py
@@ -0,0 +1,7 @@
+login = input("username: ")
+password = input("password: ")
+
+if login.lower() == "ewcia" and password == "1234":
+    print("Welcome back!")
+else:
+    print("Incorrect credentials!")
